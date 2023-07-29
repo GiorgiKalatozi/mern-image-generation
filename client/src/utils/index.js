7200cb8369762eec8c4869872d1326a291031cde
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-07-29 10:12:13 -0800

    feat: create FormField component

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -4,6 +4,7 @@ export function getRandomPrompt(prompt) {
   const randomIndex = Math.floor(Math.random() * surpriseMePrompts.length);
   const randomPrompt = surpriseMePrompts[randomIndex];
 
+  // if prompt is same return again
   if (randomPrompt === prompt) return getRandomPrompt(prompt);
 
   return randomPrompt;
