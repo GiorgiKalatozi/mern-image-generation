2d554275389fc76b8a57b19e1bf1ddbf577298e6
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-07-29 08:32:58 -0800

    chore: create random prompt util

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,3 +1,8 @@
+import { useState } from "react";
+
+import { preview } from "../assets";
+import { FormField, Loader } from "../components";
+
 export default function CreatePost() {
   return <div>CreatePost</div>;
 }
