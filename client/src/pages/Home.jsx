c0cd88ebb3e802a175f32f6468787b9dfadb411b
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-07-30 14:28:38 -0800

    chore: get posts in home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -19,6 +19,32 @@ export default function Home() {
 
   const [searchText, setSearchText] = useState("");
 
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+        const response = await fetch("http://localhost:8080/api/v1/post", {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        if (response.ok) {
+          const result = await response.json();
+          setAllPosts(result.data.reverse());
+        }
+      } catch (error) {
+        alert(error);
+        console.log(error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
   return (
     <section className="max-w-7xl mx-auto">
       <div>
@@ -50,7 +76,7 @@ export default function Home() {
               {searchText ? (
                 <RenderCards data={[]} title="No search results found" />
               ) : (
-                <RenderCards data={[]} title="No posts found" />
+                <RenderCards data={allPosts} title="No posts found" />
               )}
             </div>
           </>
