264280b6e454e234be36488da3a15cf78e2c08df
Giorgi Kalatozi <giorgi.kalatozishvili.1@btu.edu.ge>
2023-07-30 08:55:24 -0800

    chore: routes setup

--- server/index.js
+++ server/index.js
@@ -1,14 +1,20 @@
 import express from "express";
 import * as dotenv from "dotenv";
 import cors from "cors";
-dotenv.config();
 
 import connectDb from "./db/connect.js";
+import postRoutes from "./routes/post.js";
+import dalleRoutes from "./routes/dalle.js";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
+
+app.use("/api/v1/post", postRoutes);
+app.use("/api/v1/dalle", dalleRoutes);
 
 app.get("/", async (req, res) => {
   res.send("Hello from Server!");
